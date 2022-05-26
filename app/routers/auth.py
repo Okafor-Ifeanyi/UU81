15b5f246e974d0295cb42202a730706326a92ed4
Okafor Ifeanyi <zeusifeanyi058@gmail.com>
2022-05-26 09:41:05 +0100

    snapchat streaks

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -19,7 +19,7 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session =
         models.User.email == user_credentials.username).first()
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, 
                             detail= f"Invalid Credentials")
 
     if not utils.verify(user_credentials.password, user.password):
@@ -28,7 +28,6 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session =
     access_token = oauth2.create_access_token(data ={"user_id": user.id, "admin": user.admin})
     
     return {"access_token": access_token, "token_type": "bearer"}
-
 
 # Mail configuration for sending request
 conf = ConnectionConfig(
